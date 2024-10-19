6d833e6544f1962775efe1be4602fafc51cec7ab
Alexandre Tavares <alexandre.pinhotavares@gmail.com>
2024-10-19 12:13:05 +0100

    Update

--- app.go
+++ app.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<html><body><h1>Hello from go! </h1></body></html>")
+	fmt.Fprint(w, "<html><body><h1>Hello from kamal and digital ocean! </h1></body></html>")
 }
